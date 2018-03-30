816c46506e0c75c13128ba6f75d8c1c0351194da
zero-point <irinapreda91@gmail.com>
2018-03-30 20:25:05 +0100

    random forrest classifier

---
+++ improved_script.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+TARGET = "is_female"
+
+df = pd.read_csv("train.csv", low_memory=False)
+#print(df.shape)
+#print(df.head())
+
+df2 = df.dropna(axis=1)
+#print (df2.shape)
+
+# to get all columns names
+#print (df2.columns)
+
+X = df2.drop(['train_id', 'is_female'], axis=1)
+Y = df2.is_female
+
+test = pd.read_csv("test.csv", low_memory=False)
+#print(test.shape)
+
+# keep those columns from training data X
+test3 = test.reindex(columns=X.columns, fill_value=0)
+#print(test3.shape)
+
+#print (Y.shape, X.shape, test3.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.33, random_state=42)
+
+#print (X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+Ntree = 500
+clf = RandomForestClassifier(n_estimators=Ntree, random_state=1234)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+#print (cm)
+
+y_prob = clf.predict_proba(X_test)
+
+from sklearn import metrics
+
+# check which score do we need, we need column 1  not column 0
+print(y_prob)
+
+print(metrics.roc_auc_score(y_test, y_prob[:, 1]))
+
+Ntree = 500
+clf2 = RandomForestClassifier(n_estimators=Ntree, random_state=1234)
+clf2.fit(X, Y)
+y_submit = clf2.predict_proba(test3)[:, 1]
+test['is_female'] = y_submit
+ans = test[['test_id', 'is_female']]
+ans.to_csv('submit.csv', index=None)
+
+#use different classifier
+#parameter tuning using cross validation
+#missing data imputation
+#bagging and emsembing
+#deep learning neural network
+
